2e67c3fb59ec7841fdd796b3848288d8dd9f988f
RafaelMarcolinoSilva <77708442+RafaelMarcolinoSilva@users.noreply.github.com>
2022-06-17 21:52:32 -0300

    Add files via upload

---
+++ port.js
@@ -0,0 +1,3 @@
+alert("Olá, é um prazer ter você aqui visitando meu portfólio.");
+var usuario = prompt("Digite aqui o seu nome: ");
+alert(usuario +", se caso quiser entrar em contato comigo, ao final da página se encontram o meu telefone e o meu endereço de email.")
